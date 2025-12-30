14ee52b366c623fccff1323b897dfcef875f5ad1
burakasikk <buraktugrulasik@gmail.com>
2025-12-30 21:31:32 +0300

    feat: profiller için stack navigasyon kurulumu

--- App.js
+++ App.js
@@ -1,1 +1,17 @@
-console.log(process.env.EXPO_PUBLIC_API_BASE_URL);
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ProfilesListScreen from './screens/ProfilesListScreen';
+import ProfileDetailScreen from './screens/ProfileDetailScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Profiles" component={ProfilesListScreen} />
+        <Stack.Screen name="ProfileDetail" component={ProfileDetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
